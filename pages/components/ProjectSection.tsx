d27e3764c7a7892370e8b41efb93b33515d32d65
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 17:20:05 +0200

    reworking this section

--- pages/components/ProjectSection.tsx
+++ pages/components/ProjectSection.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 import picture2 from "../../public/images/travel-landing-page-featured-pic-(optimized)-2.1.jpg";
 import picture3 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
+import picture4 from "../../public/images/food-web-app-featured-pic-(optimized)-2.0.jpg";
+import picture5 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
 import React from "react";
 
 const ProjectSection = () => {
@@ -13,24 +15,6 @@ const ProjectSection = () => {
           priority={true}
           src={picture1}
           alt="project image"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image 2 */}
-      <div className="relative w-full h-[520px] md:h-[720px] py-12">
-        <Image
-          priority={true}
-          src={picture2}
-          alt="project image 2"
-          className="object-cover w-full h-full rounded-3xl"
-        />
-      </div>
-      {/* Image 3 */}
-      <div className="relative w-full h-[520px] md:h-[720px] py-12">
-        <Image
-          priority={true}
-          src={picture3}
-          alt="project image 3"
           className="object-cover w-full h-full rounded-3xl"
         />
       </div>
