df0fb56945a81dcdc64a6219cc538b05018a195e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 17:26:19 +0200

    latest

--- pages/components/ProjectSection.tsx
+++ pages/components/ProjectSection.tsx
@@ -5,18 +5,25 @@ import picture3 from "../../public/images/sneaker-head-featured-pic-(optimized)-
 import picture4 from "../../public/images/food-web-app-featured-pic-(optimized)-2.0.jpg";
 import picture5 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
 import React from "react";
+import Link from "next/link";
 
 const ProjectSection = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen px-6 py-4 lg:px-32">
-      {/* Image 1 */}
-      <div className="relative w-full h-[520px] md:h-[720px] py-12">
-        <Image
-          priority={true}
-          src={picture1}
-          alt="project image"
-          className="object-cover w-full h-full rounded-3xl"
-        />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-24">
+        {/* Project 1 */}
+        <div className="flex flex-col justify-center items-center gap-4 ">
+          <Link
+            href="/projects/furnico"
+            className="relative w-full h-[420px] md:h-[720px]"
+          />
+          <Image
+            priority={true}
+            src={picture1}
+            alt="project image 1"
+            className="w-full h-full object-cover rounded-3xl hover:scale-90"
+          />
+        </div>
       </div>
     </section>
   );
