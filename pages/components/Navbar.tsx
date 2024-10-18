528087912d5e10bd6c5ba9e3287585d7b5671fd8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 16:03:24 +0200

    initial navbar for now

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="max-w-screen-3xl mx-auto flex justify-between items-center w-full h-20 px-6 py-4 lg:px-32">
-      Navbar
+      <Link
+        href="/"
+        prefetch={false}
+        className="text-xl font-bold text-wrap hover:underline"
+      >
+        Darko Mihajlovski
+      </Link>
+      <Link
+        href="/about"
+        prefetch={false}
+        className="text-xl font-semibold hover:underline"
+      >
+        About
+      </Link>
     </nav>
   );
 };
