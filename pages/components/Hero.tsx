fe331af6773099b82c53e60284eaa8ed9ed61be7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 16:11:55 +0200

    working h1 for now

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main>
+      Hero
+      <div className="">
+        {/* Working Title */}
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+          Web Development with a focus on the Front End and React
+        </h1>
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
