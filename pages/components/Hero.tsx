1daacb865a85303540da0db601621fb68d32f3a1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 16:23:01 +0200

    latest

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="flex flex-col justify-center items-start w-full h-full">
         {/* Working Title */}
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
-          Web Development With A Focus On The Front End And React.js
+          Web Development. Front End. React. Design Systems & More
         </h1>
       </div>
     </main>
