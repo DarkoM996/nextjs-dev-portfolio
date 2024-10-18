9ff522ac0866229c30a2cd3ffda82752b7b79f9a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 16:59:57 +0200

    initial work + case added

--- pages/components/ProjectSection.tsx
+++ pages/components/ProjectSection.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 import React from "react";
 
 const ProjectSection = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen px-6 py-4 lg:px-32">
-      ProjectSection
+      {/* Image 1 */}
+      <div className="relative w-full h-[520px] md:h-[720px] py-12">
+        <Image
+          priority={true}
+          src={picture1}
+          alt="project image"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
     </section>
   );
 };
