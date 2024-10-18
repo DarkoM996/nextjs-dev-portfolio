7d65ea22e0978ac3af42d3e633b956f009321837
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 18:54:48 +0200

    latest for now

--- pages/components/ProjectSection.tsx
+++ pages/components/ProjectSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 import picture2 from "../../public/images/travel-landing-page-featured-pic-(optimized)-2.1.jpg";
 import picture3 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
-import picture4 from "../../public/images/food-web-app-featured-pic-(optimized)-2.0.jpg";
+import picture4 from "../../public/images/food-web-app-featured-pic-(optimized)-2.2.jpg";
 import picture5 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
 import React from "react";
 import Link from "next/link";
@@ -25,10 +25,8 @@ const ProjectSection = () => {
             />
           </Link>
           <div className="flex flex-col justify-center items-center p-4 gap-2">
-            <h4 className="text-2xl font-semibold text-primary_backgroundDarkerColor-800">
-              Furnico
-            </h4>
-            <p className="text-xl font-light text-primary_customGray-700">
+            <h4 className="text-2xl font-semibold text-neutral-900">Furnico</h4>
+            <p className="text-xl font-light text-neutral-700">
               Ecommerce Full Stack Web App
             </p>
           </div>
@@ -47,10 +45,10 @@ const ProjectSection = () => {
             />
           </Link>
           <div className="flex flex-col justify-center items-center p-4 gap-2">
-            <h4 className="text-2xl font-semibold text-primary_backgroundDarkerColor-800">
+            <h4 className="text-2xl font-semibold text-neutral-900">
               TravelLife
             </h4>
-            <p className="text-xl font-light text-primary_customGray-700">
+            <p className="text-xl font-light text-neutral-700">
               Travel Website Landing Page
             </p>
           </div>
@@ -72,9 +70,49 @@ const ProjectSection = () => {
             <h4 className="text-2xl font-semibold text-primary_backgroundDarkerColor-700">
               SneakerHead
             </h4>
-            <p className="text-xl font-light text-primary_customGray-700">
+            <p className="text-xl font-light text-neutral-700">
               Travel Website Landing Page
             </p>
+          </div>
+        </div>
+        {/* Project 4 */}
+        <div className="flex flex-col justify-center items-center gap-4 md:mb-10">
+          <Link
+            href="/projects/coffeebar"
+            className="w-full h-[420px] md:h-[720px] lg:h-[920px]"
+          >
+            <Image
+              priority={true}
+              src={picture4}
+              alt="project image 4"
+              className="w-full h-full object-cover rounded-3xl hover:scale-90 duration-500 transition-transform"
+            />
+          </Link>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <h4 className="text-2xl font-semibold text-primary_backgroundDarkerColor-700">
+              The Tomato
+            </h4>
+            <p className="text-xl font-light text-neutral-700">Food Web App</p>
+          </div>
+        </div>
+        {/* Project 5 */}
+        <div className="flex flex-col justify-center items-center gap-4 mb-40">
+          <Link
+            href="/projects/furnico"
+            className="relative w-full h-[420px] md:h-[720px] lg:h-[920px]"
+          >
+            <Image
+              priority={true}
+              src={picture5}
+              alt="project image 1"
+              className="w-full h-full object-cover rounded-3xl hover:scale-90 duration-500 transition-transform "
+            />
+          </Link>
+          <div className="flex flex-col justify-center items-center p-4 gap-2">
+            <h4 className="text-2xl font-semibold text-neutral-900">
+              The Tomato
+            </h4>
+            <p className="text-xl font-light text-neutral-700">Food Web App</p>
           </div>
         </div>
       </div>
