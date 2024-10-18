6f078defd364b5d27037d3def6e4f4ed710f9510
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 16:16:10 +0200

    working section for now

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <main>
-      Hero
-      <div className="">
+    <main className="max-w-screen-3xl mx-auto w-full h-[60vh] relative px-6 py-4 lg:px-32">
+      <div className="flex flex-col justify-center items-start w-full h-full">
         {/* Working Title */}
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
           Web Development with a focus on the Front End and React
