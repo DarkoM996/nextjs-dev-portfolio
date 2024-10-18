cf73720f0bdc20a3da976ec1a64074d84c6c8c2c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 17:16:45 +0200

    project 3 added

--- pages/components/ProjectSection.tsx
+++ pages/components/ProjectSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import picture1 from "../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 import picture2 from "../../public/images/travel-landing-page-featured-pic-(optimized)-2.1.jpg";
+import picture3 from "../../public/images/sneaker-head-featured-pic-(optimized)-2.5.jpg";
 import React from "react";
 
 const ProjectSection = () => {
@@ -21,6 +22,15 @@ const ProjectSection = () => {
           priority={true}
           src={picture2}
           alt="project image 2"
+          className="object-cover w-full h-full rounded-3xl"
+        />
+      </div>
+      {/* Image 3 */}
+      <div className="relative w-full h-[520px] md:h-[720px] py-12">
+        <Image
+          priority={true}
+          src={picture3}
+          alt="project image 3"
           className="object-cover w-full h-full rounded-3xl"
         />
       </div>
