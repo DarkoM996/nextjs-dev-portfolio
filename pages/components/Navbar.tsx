1de85613b575691407da38c87204eed7116ae17a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-18 15:59:21 +0200

    initial work

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="max-w-screen-3xl mx-auto flex justify-between items-center w-full h-20 px-6 py-4 lg:px-32">
+      Navbar
+    </nav>
+  );
 };
 
 export default Navbar;
