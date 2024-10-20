8e03d351f8f8b934eeb6ec6aa28373254fdfbd7a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:32:36 +0200

    latest

--- pages/project-components/travellife/TravelLifeHero.tsx
+++ pages/project-components/travellife/TravelLifeHero.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Head from "next/head";
 
 const TravelLifeHero = () => {
-  return <div>TravelLifeHero</div>;
+  return (
+    <>
+      <Head>
+        <title>Darko | Front End Developer</title>
+        <meta />
+      </Head>
+    </>
+  );
 };
 
 export default TravelLifeHero;
