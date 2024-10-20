e4dcfaf4680d5e49e7a99656d0973b51a5462901
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:19:47 +0200

    latest

--- pages/project-components/furnico/FurnicoImages.tsx
+++ pages/project-components/furnico/FurnicoImages.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 import picture1 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 import picture2 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.3.jpg";
-import picture3 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
-import picture4 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture3 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.1.jpg";
+import picture4 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.2.jpg";
 import picture5 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
-import picture6 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture6 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.6.jpg";
 import picture7 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
 
 const FurnicoImages = () => {
