cb677d51c41b1c3141436359214a7bb2b1ae16d0
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:44:44 +0200

    case almost done

--- pages/projects/travellife.tsx
+++ pages/projects/travellife.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Head from "next/head";
 import TravelLifeHero from "../project-components/travellife/TravelLifeHero";
 import TravelLifeText from "../project-components/travellife/TravelLifeText";
+import TravelLifeImages from "../project-components/travellife/TravelLifeImages";
+import BackToAllProjects from "../components/BackToAllProjects";
+import Footer from "../components/Footer";
 
 const travellife = () => {
   return (
@@ -12,6 +15,9 @@ const travellife = () => {
       </Head>
       <TravelLifeHero />
       <TravelLifeText />
+      <TravelLifeImages />
+      <BackToAllProjects />
+      <Footer />
     </>
   );
 };
