a49e20161a1d5051f1c86a1bcb4add8450dc5b31
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 20:18:30 +0200

    latest changes

--- pages/project-components/coffeebar/CoffeeBarImages.tsx
+++ pages/project-components/coffeebar/CoffeeBarImages.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import picture1 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
-import picture2 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.3.jpg";
-import picture3 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.1.jpg";
-import picture4 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.2.jpg";
-import picture5 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
-import picture6 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.6.jpg";
-import picture7 from "../../../public/images/furniturewebsite-featured-pic(optimized)-2.4.jpg";
+import picture1 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.3.jpg";
+import picture2 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.4.jpg";
+import picture3 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.0.jpg";
+import picture4 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.5.jpg";
+import picture5 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.3.jpg";
+import picture6 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.4.jpg";
+import picture7 from "../../../public/images/coffee-bar-concept-version-2.0-volume-2.3.jpg";
 
 const CoffeeBarImages = () => {
   return (
--- pages/projects/coffeebar.tsx
+++ pages/projects/coffeebar.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import CoffeeBarHero from "../project-components/coffeebar/CoffeeBarHero";
 import CoffeeBarText from "../project-components/coffeebar/CoffeeBarText";
 import CoffeeBarImages from "../project-components/coffeebar/CoffeeBarImages";
+import BackToAllProjects from "../components/BackToAllProjects";
+import Footer from "../components/Footer";
 
 const coffeebar = () => {
   return (
@@ -14,6 +16,8 @@ const coffeebar = () => {
       <CoffeeBarHero />
       <CoffeeBarText />
       <CoffeeBarImages />
+      <BackToAllProjects />
+      <Footer />
     </>
   );
 };
