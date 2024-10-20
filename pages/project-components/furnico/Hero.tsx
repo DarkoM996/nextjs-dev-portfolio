02b90fcd0825eb7939abdd1120846acc9b8c67fb
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:00:04 +0200

    latest

---
+++ pages/project-components/furnico/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="max-w-screen-md mx-auto w-full h-full relative px-6 py-6">
+      <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+          Furnico Web App
+        </h1>
+        <h3 className="text-2xl md:text-3xl font-medium text-primary_backgroundDarkerColor-600">
+          Furnico is an ecommerce web app that relies on clean and minimalistic
+          design with a brown color palette to create a warm and cozy feeling
+          when the user is on the website.
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ pages/projects/furnico.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import FurnicoHero from "../project-components/furnico/FurnicoHero";
+
+const furnico = () => {
+  return (
+    <>
+      <Head>
+        <title>Darko | Front End Developer</title>
+        <meta />
+      </Head>
+      <FurnicoHero />
+    </>
+  );
+};
+
+export default furnico;
