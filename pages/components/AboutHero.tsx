8d3035147cd144a7ba213b09d51df3288be7ce69
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 15:50:23 +0200

    latest

--- pages/components/AboutHero.tsx
+++ pages/components/AboutHero.tsx
@@ -5,7 +5,8 @@ const AboutHero = () => {
     <section className="max-w-screen-md mx-auto w-full h-full relative px-6 py-6">
       <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
-          UX Engineer working on user-center innovation and product evolution
+          Front End Developer working on user-center innovation and product
+          evolution
         </h1>
       </div>
     </section>
