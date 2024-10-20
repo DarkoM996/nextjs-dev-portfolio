0c181c89eef6a6fbf629aaa4ed3348405c6c1f66
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 20:26:51 +0200

    new case added + hero

---
+++ pages/project-components/thetomato/TheTomatoHero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TheTomatoHero = () => {
+  return (
+    <section className="max-w-screen-md mx-auto w-full h-full relative px-6 py-4">
+      <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+          TheTomato
+        </h1>
+        <h3 className="text-2xl md:text-3xl font-medium text-primary_backgroundDarkerColor-600">
+          The Tomato is a food web app that's designed to be easy to use with a
+          clean and minimalistic design. The brand's goals are to make ordering
+          food an enjoyable and seamless process.
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default TheTomatoHero;
---
+++ pages/projects/thetomato.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const thetomato = () => {
+  return (
+    <>
+      <Head>
+        <title>Darko | Front End Developer</title>
+        <meta />
+      </Head>
+    </>
+  );
+};
+
+export default thetomato;
