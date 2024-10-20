cfeff8cf9e3e6dad5c324896add66aee3e83eb20
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 14:38:43 +0200

    Footer done for now

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Youtube, YoutubeIcon } from "lucide-react";
+import { FaBehance, FaDribbble, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-      <YoutubeIcon />
+    <div className="max-w-screen-3xl mx-auto w-full h-full relative px-6 py-4 lg:px-32">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-6">
+        <p className="text-primary_customGray-700">&copy; Darko Mihajlovski</p>
+        {/* Icons with links pointing out to socials */}
+        <div className="flex items-center gap-8 text-2xl">
+          <a href="https://github.com/DarkoM996">
+            <FaGithub />
+          </a>
+          <a href="https://www.behance.net/darkomihajlo">
+            <FaBehance />
+          </a>
+          <a href="https://dribbble.com/DarkoM_0-10">
+            <FaDribbble />
+          </a>
+          <a href="https://www.linkedin.com/in/darko-mihajlovski-1bb4701b7">
+            <FaLinkedinIn />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
