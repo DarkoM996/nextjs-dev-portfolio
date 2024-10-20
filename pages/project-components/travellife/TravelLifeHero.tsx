c1ab6f5d074628639cbbe3a17e6992e4b5ef6f55
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:37:06 +0200

    latest

--- pages/project-components/travellife/TravelLifeHero.tsx
+++ pages/project-components/travellife/TravelLifeHero.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const TravelLifeHero = () => {
-  return <div>TravelLifeHero</div>;
+  return (
+    <section className="max-w-screen-md mx-auto w-full h-full relative px-6 py-6">
+      <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+          TravelLife
+        </h1>
+        <h3 className="text-2xl md:text-3xl font-medium text-primary_backgroundDarkerColor-600">
+          TravelLife is a landing page for a travel agency that relies on clean
+          as well as minimalistic design with clean typography and device
+          friendly layout that aims to capture its users attentions.
+        </h3>
+      </div>
+    </section>
+  );
 };
 
 export default TravelLifeHero;
