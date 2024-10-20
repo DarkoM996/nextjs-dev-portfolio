26b22bcc673fa1e789bf2590125b86f44d206433
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-20 16:36:04 +0200

    new project case

---
+++ pages/projects/travellife.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+import TravelLifeHero from "../project-components/travellife/TravelLifeHero";
+
+const travellife = () => {
+  return (
+    <>
+      <Head>
+        <title>Darko | Front End Developer</title>
+        <meta />
+      </Head>
+      <TravelLifeHero />
+    </>
+  );
+};
+
+export default travellife;
