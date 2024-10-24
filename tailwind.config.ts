896e09df7f9a7d4887810f9ada4e2ad8a92afe34
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-24 15:56:28 +0200

    latest

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,46 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class", // add this line
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // add this line
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_backgroundLighterColor: {
+          50: "#FAFAFA",
+          100: "#F1F1F1",
+          200: "#EDEDED",
+          300: "#E6E6E6",
+          400: "#E0E0E0",
+          500: "#D9D9D9",
+        },
+        primary_customGray: {
+          50: "#BFBFBF",
+          100: "#B3B3B3",
+          200: "#A6A6A6",
+          300: "#999999",
+          400: "#8C8C8C",
+          500: "#808080",
+          600: "#737373",
+          700: "#666666",
+          800: "#595959",
+          900: "#4D4D4D",
+          950: "#404040",
+        },
+        primary_backgroundDarkerColor: {
+          600: "#262626",
+          700: "#1F1F1F",
+          800: "#191919",
+          850: "#121212",
+          900: "#0D0C0C",
+          950: "#050505",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
