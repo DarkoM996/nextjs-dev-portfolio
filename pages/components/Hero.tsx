d07117a5b40f27a1888c32b455a532108f77feb6
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-24 23:28:20 +0200

    latest

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -20,10 +20,10 @@ const Hero = () => {
   };
 
   return (
-    <main className="max-w-screen-2xl mx-auto w-full h-[60vh] relative px-6 py-4">
+    <main className="max-w-screen-2xl mx-auto w-full h-[60vh] relative px-4 py-4 md:px-6">
       <div className="flex flex-col justify-center items-center w-full h-[80%] gap-8">
         {/* Working Title */}
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
           Web Development. Front End. React. Design Systems & More
         </h1>
         <button className="hover:underline text-2xl p-2" onClick={toggleTheme}>
