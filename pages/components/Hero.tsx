5bca16e4529fcbd6e2a47e6013980719dd83d8a7
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 16:10:34 +0200

    test correction

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
     <main className="max-w-screen-2xl mx-auto w-full h-[60vh] relative px-4 py-4 md:px-6">
       <div className="flex flex-col justify-center items-center w-full h-[80%] gap-8">
         {/* Working Title */}
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
           Web Development. Front End. React. Design Systems & More
           <button
             className="absolute bottom-0 right-0 mr-6 mb-6 hover:underline text-xl p-4 bg-black text-white rounded-full"
