8f2981a5993489a2fd32076ae3cc982f3e24356e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 15:31:53 +0200

    latest

--- pages/project-components/furnico/FurnicoText.tsx
+++ pages/project-components/furnico/FurnicoText.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FurnicoText = () => {
   return (
     <section className="max-w-screen-md mx-auto w-full h-full flex flex-col justify-center items-start gap-6 px-6 py-8">
-      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500 ">
+      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500">
         Furnico is an ecommerce full stack web app that I designed in Figma and
         for which I made mockups in Photoshop as well. I decided to turn this
         project into a full stack ecommerce web app that I'm building in Mongo
@@ -11,7 +11,7 @@ const FurnicoText = () => {
         well underway but today, the case study will talk about the design and
         the decisions relating to this process.
       </p>
-      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500  ">
+      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500">
         Furnico started as a new and fresh take on designing an ecommerce web
         app for a fictional brand. The goal was to create a well designed and
         visiually stunning website that is easy to use. To achieve this, I opted
@@ -19,7 +19,7 @@ const FurnicoText = () => {
         images with rounded corners, buttons etc so the user can have a warm and
         cosy feeling while navigating through the web app.
       </p>
-      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500  ">
+      <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500">
         For now, I only have the designs to show. I made the mockups in
         Photoshop to showcase this design. The process of bringing this app to
         the web is underway with the usage of the MERN stack. Until that is
