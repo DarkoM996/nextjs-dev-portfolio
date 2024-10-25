3ec9c0528c963f4b6db36bb9c2ef1e97fa0dc775
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 15:21:58 +0200

    removing

--- pages/components/DarkModeButton.tsx
+++ pages/components/DarkModeButton.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import { useState } from "react";
-import { useTheme } from "next-themes";
-import { MoonIcon, SunIcon } from "lucide-react";
-const iconStyles = "h-4 w-4 text-gray-600 dark:text-gray-300";
 
 export default function DarkModeButton() {
   const [darkMode, setDarkMode] = useState<boolean>(false);
