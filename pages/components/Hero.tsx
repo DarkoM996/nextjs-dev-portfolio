42dbb11acc6ff5c9416070942ccc72fe76c848af
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 16:18:29 +0200

    test

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -19,13 +19,13 @@ const Hero = () => {
   };
 
   return (
-    <main className="max-w-screen-2xl mx-auto w-full h-[80vh] lg:h-[60vh] relative px-4 py-4 md:px-6">
+    <main className="max-w-screen-2xl mx-auto w-full h-[60vh] relative px-4 py-4 md:px-6">
       <div className="flex flex-col justify-center items-center w-full h-[80%] gap-8">
         {/* Working Title */}
         <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold">
           Web Development. Front End. React. Design Systems & More
           <button
-            className="absolute bottom-0 right-0 mr-6 mb-6 hover:underline text-xl p-4 bg-black text-white rounded-full"
+            className="absolute bottom-0 right-0 mr-6 mb-20 lg:mr-6 lg:mb-6 hover:underline text-xl p-4 bg-black text-white rounded-full"
             onClick={toggleTheme}
           >
             Cool Effect
