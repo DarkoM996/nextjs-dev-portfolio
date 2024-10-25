03c290d1c40c90a31dc530f5d341630e1eea49bc
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 17:14:16 +0200

    design correction

--- pages/components/AboutHero.tsx
+++ pages/components/AboutHero.tsx
@@ -4,7 +4,7 @@ const AboutHero = () => {
   return (
     <section className="max-w-screen-md mx-auto w-full h-full relative px-6 py-6 text:bg-primary_backgroundDarkerColor-950 dark:bg-primary_backgroundDarkerColor-950">
       <div className="flex flex-col justify-center items-start gap-6 lg:gap-16 lg:pt-16">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-medium">
           Front End Developer working on user-center innovation and product
           evolution
         </h1>
