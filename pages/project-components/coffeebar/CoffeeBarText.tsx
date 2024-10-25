eb6e3984fe170c25b45e48cde4d8b82ab00d12ed
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-25 15:43:31 +0200

    test

--- pages/project-components/coffeebar/CoffeeBarText.tsx
+++ pages/project-components/coffeebar/CoffeeBarText.tsx
@@ -53,7 +53,7 @@ const CoffeeBarText = () => {
       </p>
       <p className="text-xl leading-normal text-primary_customGray-900  dark:text-primary_customGray-500">
         There were some signs of a potential investment, but{" "}
-        {`that'd be at least`}a decade away if not more. One option would be to
+        {`that'd be at least a`}decade away if not more. One option would be to
         offer a way to book a table at a customer specific wanted time etc.
         Overall duo to unique coffee drinking culture, expensive distruction and
         just not enough interest, the Starbucks like business method is not
